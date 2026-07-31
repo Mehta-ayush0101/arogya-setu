cfb5ae77ea2078b3b4cfc3547d5da53d36d42bc1
Mehta-ayush0101 <mehtaayush20000@gmail.com>
2026-07-31 22:39:30 +0530

    feat: add backend/src/routes/emergency.ts

---
+++ backend/src/routes/emergency.ts
@@ -0,0 +1,8 @@
+// Re-export all routes from main files
+export { emergencyRouter } from './medicines'
+export { notificationsRouter } from './medicines'
+export { analyticsRouter } from './medicines'
+export { patientsRouter } from './appointments'
+export { doctorsRouter } from './appointments'
+export { phcRouter } from './appointments'
+export { usersRouter } from './appointments'
